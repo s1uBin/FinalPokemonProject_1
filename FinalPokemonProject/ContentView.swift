25ba8ed1ce37f80a4ca66959e0e53029f3df81e1
WeiBin <binwong112@gmail.com>
2020-01-05 16:48:00 +0800

    Initial Commit

---
+++ FinalPokemonProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinalPokemonProject
+//
+//  Created by user on 5/1/2020.
+//  Copyright © 2020 00557052. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
